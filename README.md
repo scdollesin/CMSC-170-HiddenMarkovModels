d442bc367a28410aa6f397ce5cb5237330ce4123
scdollesin <scdollesin@up.edu.ph>
2023-11-28 01:50:10 +0800

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CMSC-170-HiddenMarkovModels
+AUTHOR: Samantha Shane C. Dollesin \
+STUDENT NO.: 2020-01893 \
+SECTION: WX-1L
+
+## PROGRAM DESCRIPTION: 
+Hidden Markov Model
+
+## Program Requirements
+* Python
--- hmm.py
+++ hmm.py
@@ -1,38 +1,44 @@
+# EXERCISE #9 HIDDEN MARKOV MODELS
+# AUTHOR: Samantha Shane C. Dollesin
+# STUDENT NO.: 2020-01893
+# SECTION: WX-1L
+
 input_file = open("hmm.in", "r")
 sequences = []
 values = []
 measurements = []
 measurement_probs = {}
-state_probs = {}
+transition_probs = {}
 cases = []
 
+#This is a recursive function that calculates for P(s) using total probability
 def getPS(s):
     #print(f"P({s})")
-    if s in state_probs.keys():
+    if s in transition_probs.keys():     #if the value for P(s) has already been computed, return the value
         #print(f"return P({s})")
-        return state_probs[s]
-    else:
-        state_probs[s] = 0
-        for i in list(state_probs):
+        return transition_probs[s]
+    else:                           #otherwise, solve for the value using recursion
+        transition_probs[s] = 0
+        for i in list(transition_probs):
             if i.startswith(s[0]):
                 if not i[-1].isdigit():
-                    state_probs[s] = state_probs[s] + state_probs[i]*getPS(i[1]+str(int(s[1:])-1))
+                    transition_probs[s] = transition_probs[s] + transition_probs[i]*getPS(i[1]+str(int(s[1:])-1))
                     if(s[0] == values[0]):
-                        state_probs[values[1]+s[1:]] = 1 - state_probs[s]
-                        #print(f"{values[1]+s[1:]}: {state_probs[values[1]+s[1:]]}")
+                        transition_probs[values[1]+s[1:]] = 1 - transition_probs[s]
+                        #print(f"{values[1]+s[1:]}: {transition_probs[values[1]+s[1:]]}")
                     else:
-                        state_probs[values[0]+s[1:]] = 1 - state_probs[s]
-                        #print(f"{values[0]+s[1:]}: {state_probs[values[0]+s[1:]]}")
-                    #print(f"{s}: {state_probs[s]}")
+                        transition_probs[values[0]+s[1:]] = 1 - transition_probs[s]
+                        #print(f"{values[0]+s[1:]}: {transition_probs[values[0]+s[1:]]}")
+                    #print(f"{s}: {transition_probs[s]}")
 
-        return state_probs[s]
+        return transition_probs[s]
 
 def getPM(m):
     PM = 0
     for i in list(measurement_probs):
             if i.startswith(m[0]):
-                PM = PM + measurement_probs[i]*state_probs[i[1]+m[1:]]
-                #print(f"+ {measurement_probs[i]}*{state_probs[i[1]+m[1]]}")
+                PM = PM + measurement_probs[i]*transition_probs[i[1]+m[1:]]
+                #print(f"+ {measurement_probs[i]}*{transition_probs[i[1]+m[1]]}")
     return PM
         
 if (input_file.readable()):
@@ -59,36 +65,38 @@ if (input_file.readable()):
     numCases = int(input_file.readline().strip()) 
     for i in range(numCases):
         cases.append(input_file.readline().strip().split(" given "))
-    print(f"Cases: {cases}")
+    print(f"Cases: {cases}\n")
 
     input_file.close()
     output = open("hmm.out", "w")
 
-    #determine probabilities for each possible state
-    #TODO: replace sequences[0] with loop
+    #determine probabilities for each possible transition
     for sequence in sequences:
-        state_probs = {}
+        print(f"Sequence: {sequence}")
+        transition_probs = {}
         for i in range(len(values)):
             for j in range(len(values)):
-                a = 0
-                b = 0
+                b = 0   #contains the total number of S or T with a next state
+                a = 0   #contains the number of S or T followed by a given value out of b occurances
                 for k in range(len(sequence)-1):
-                    if(sequence[k] == values[j]):
+                    if(sequence[k] == values[j]):   
                         b += 1
                         if(sequence[k+1] == values[i]):
                             a+=1
                 #print(f"{values[i]+values[j]}: {a}/{b} = {a/b}")
-                state_probs[values[i]+values[j]] = a/b
-        #determine probability for each possible starting state
+                transition_probs[values[i]+values[j]] = a/b
+        #determine probability for possible starting states
         for i in range(len(values)):
-            state_probs[values[i]+"0"] = 1 if sequence[0] == values[i] else 0
-        print(f"State Probabilities: {state_probs}")
+            transition_probs[values[i]+"0"] = 1 if sequence[0] == values[i] else 0
+        #print(f"Transition Probabilities: {transition_probs}")
         
+        #compute for each case and write results to output file
         output.write(sequence + "\n")
-        case_probs = {}
+        case_probs = {}   #stores the value computed for each case
         for i in range(len(cases)):
             case_probs[i] = (measurement_probs[cases[i][1][0]+cases[i][0][0]]*getPS(cases[i][0])) / getPM(cases[i][1])
             #print(f"({measurement_probs[cases[i][1][0]+cases[i][0][0]]} * {str(getPS(cases[i][0]))[:6]}) / {str(getPM(cases[i][1]))[:6]} = {str(case_probs[i])[:6]}")
             output.write(f"{cases[i][0]} given {cases[i][1]} = {str(case_probs[i])[:6]}\n")
-        
+        print(f"Transition Probabilities: {transition_probs}\n")
+
     output.close()
